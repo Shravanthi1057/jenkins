3b4cd77e046654c28a7a2e5629e1c96eca02ca77
Shravanthi1057 <gnsshrav658@gmail.com>
2025-04-22 13:57:48 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,3 +8,35 @@ public class App {
         System.out.println("Hello World!");
     }
 }
+
+
+pipeline 
+{
+    agent any
+    stages
+    {
+        stage('Checkout') 
+        {
+            steps
+            {
+                git branch: 'main', url: 'https://github.com/cmritsarada/exp6.git' // Replace with your repository                  URL, replace #39 with single quote
+            }
+        }
+
+        stage('Build') 
+        {
+            steps 
+            {
+                 sh 'mvn clean package'
+            }
+        }
+        
+        stage('Test') 
+        {
+            steps 
+            {
+                  sh 'mvn test'
+            }    
+        }    
+    }
+}
